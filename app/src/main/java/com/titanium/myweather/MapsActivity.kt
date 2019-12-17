c987212e9249798454b931a7cd1bf243d5edfc6f
Titanium <titanbest@gmail.com>
2019-12-17 18:35:21 +0200

    create a new project

---
+++ app/src/main/java/com/titanium/myweather/MapsActivity.kt
@@ -0,0 +1,12 @@
+package com.titanium.myweather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
